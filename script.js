5045b039549bd3838293ee8355398280168513f4
Meghabhatt23 <meghabhatt241@gmail.com>
2025-02-07 12:40:12 -0700

    made some changes

--- script.js
+++ script.js
@@ -5,3 +5,15 @@ menu.onclick = () => {
     menu.classList.toggle('bx-x');
     nav.classList.toggle('active');
 }
+function wrapWordsInSpans(selector) {
+    const elements = document.querySelectorAll(selector);
+    elements.forEach(element => {
+        const words = element.textContent.split(' ');
+        element.innerHTML = words.map(word => `<span>${word}</span>`).join(' ');
+    });
+}
+
+// Apply this function to all your text elements
+wrapWordsInSpans('.info-box h1');
+wrapWordsInSpans('.info-box h3');
+wrapWordsInSpans('.info-box p');
