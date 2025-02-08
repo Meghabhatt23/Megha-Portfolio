e50de4e72ae5c0d01e81035720a0c06861a66304
Meghabhatt23 <meghabhatt241@gmail.com>
2025-02-08 13:43:17 -0700

    responsive menu

--- script.js
+++ script.js
@@ -5,4 +5,8 @@ menu.onclick = () => {
     menu.classList.toggle('bx-x');
     nav.classList.toggle('active');
 }
-
+AOS.init();
+menu.onclick = () => {
+    menu.classList.toggle('bx-x'); // Toggle icon change
+    nav.classList.toggle('active'); // Show/hide menu
+};
