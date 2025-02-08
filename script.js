a74d74c15f4b701beb85b3bea916f1310c034db9
Meghabhatt23 <meghabhatt241@gmail.com>
2025-02-08 13:47:48 -0700

    made changes

--- script.js
+++ script.js
@@ -2,11 +2,7 @@ const menu = document.querySelector("#menu");
 const nav = document.querySelector(".links");
 
 menu.onclick = () => {
-    menu.classList.toggle('bx-x');
-    nav.classList.toggle('active');
-}
-AOS.init();
-menu.onclick = () => {
     menu.classList.toggle('bx-x'); // Toggle icon change
     nav.classList.toggle('active'); // Show/hide menu
 };
+AOS.init();
