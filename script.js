bd05ad0a8dce6d07d3e9f199c9ee2b230b063a42
Meghabhatt23 <meghabhatt241@gmail.com>
2025-02-08 20:48:05 -0700

    responsive menu working 90%

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 const menu = document.querySelector("#menu");
 const navLinksContainer = document.querySelector(".links");
+const nav = document.querySelector("nav"); // Add this line to select the nav element
 
 menu.onclick = () => {
   menu.classList.toggle('bx-x');
-  nav.classList.toggle('active');
-
+  nav.classList.toggle('active'); // Make sure the class is toggled on the nav element
 }
+
 window.addEventListener("scroll", function () {
   let sections = document.querySelectorAll("section");
   let navLinks = document.querySelectorAll(".links a");
@@ -21,5 +22,5 @@ window.addEventListener("scroll", function () {
     }
   });
 });
-AOS.init(); 
 
+AOS.init();
